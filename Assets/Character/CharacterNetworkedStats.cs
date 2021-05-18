876359cbd2fb420db9159021a736c1d895f392f9
Gabriel <circiu_gabriel@outlook.com>
2021-05-18 21:07:07 +0200

    Basic player position and rotation sync

--- Assets/Character/CharacterNetworkedStats.cs
+++ Assets/Character/CharacterNetworkedStats.cs
@@ -10,7 +10,8 @@ public class CharacterNetworkedStats : MonoBehaviour
     private ulong _localSteamID, _receivedSteamID;
     private string _dataString, _receivedDataString;
     private Transform _localPlayerTransform;
-    private Vector3 _localPlayerVector3, _receivedPlayerVector3;
+    private Vector3 _localPlayerPositionVector, _receivedPlayerPositionVector;
+    private float _localPlayerRotationVectorY, _receivedPlayerRotationVectorY;
 
     // ASCII: N - No save, P - Player, P - Position (send ID, position, rotation, etc.)
     private const string DataIdentifier = "NPP";
@@ -24,16 +25,20 @@ public class CharacterNetworkedStats : MonoBehaviour
         _localSteamID = SteamClient.SteamId.Value;
         _localPlayerTransform = GetComponent<Transform>();
 
-        InvokeRepeating(nameof(SendNetworkedCharacterData), 5.0f, 1.0f);
+        InvokeRepeating(nameof(SendNetworkedCharacterData), 3.0f, 0.1f);
     }
 
     private void SendNetworkedCharacterData()
     {
-        _localPlayerVector3 = _localPlayerTransform.position;
-        _dataString = DataIdentifier + _localSteamID + 
-                      _localPlayerVector3.x.ToString("+0000.00;-0000.00") +
-                      _localPlayerVector3.y.ToString("+0000.00;-0000.00") +
-                      _localPlayerVector3.z.ToString("+0000.00;-0000.00");
+        _localPlayerPositionVector = _localPlayerTransform.position;
+        _localPlayerRotationVectorY = _localPlayerTransform.rotation.eulerAngles.y;
+
+        _dataString = DataIdentifier + _localSteamID +
+                      _localPlayerPositionVector.x.ToString("+0000.00;-0000.00") +
+                      _localPlayerPositionVector.y.ToString("+0000.00;-0000.00") +
+                      _localPlayerPositionVector.z.ToString("+0000.00;-0000.00") +
+                      _localPlayerRotationVectorY.ToString("000.0");
+
         var dataArray = Encoding.UTF8.GetBytes(_dataString);
         _steamManager.SendMessageToSocketServer(dataArray);
     }
@@ -42,12 +47,17 @@ public class CharacterNetworkedStats : MonoBehaviour
     {
         // Change a character position based on ID.
         _receivedSteamID = ulong.Parse(Encoding.UTF8.GetString(dataArray, 3, 17));
-        _receivedPlayerVector3.x = float.Parse(Encoding.UTF8.GetString(dataArray, 20, 8));
-        _receivedPlayerVector3.y = float.Parse(Encoding.UTF8.GetString(dataArray, 28, 8));
-        _receivedPlayerVector3.z = float.Parse(Encoding.UTF8.GetString(dataArray, 36, 8));
-        _clientDataManager.players[_receivedSteamID].character.transform.SetPositionAndRotation(_receivedPlayerVector3, Quaternion.identity);
+        
+        _receivedPlayerPositionVector.x = float.Parse(Encoding.UTF8.GetString(dataArray, 20, 8));
+        _receivedPlayerPositionVector.y = float.Parse(Encoding.UTF8.GetString(dataArray, 28, 8));
+        _receivedPlayerPositionVector.z = float.Parse(Encoding.UTF8.GetString(dataArray, 36, 8));
+        _receivedPlayerRotationVectorY = float.Parse(Encoding.UTF8.GetString(dataArray, 44, 5));
 
-        _receivedDataString = Encoding.UTF8.GetString(dataArray, 20, dataArray.Length - 20);
+        var characterTransform = _clientDataManager.players[_receivedSteamID].character.transform;
+        characterTransform.position = _receivedPlayerPositionVector;
+        characterTransform.rotation = Quaternion.Euler(0f, _receivedPlayerRotationVectorY, 0f);
+
+        _receivedDataString = Encoding.UTF8.GetString(dataArray, 0, dataArray.Length);
         Debug.Log($"CLIENT: Received character data...\nID: {_receivedSteamID}, Pos: {_receivedDataString}\n");
     }
 }
